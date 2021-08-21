bef240984dc2519850675f3e96267edd012b3f61
Sachin Dilshan <sachin_n@epiclanka.net>
2021-08-21 17:13:07 +0530

    bugfix

--- src/main/java/net/smartplan/fitness/service/impl/UserServiceImpl.java
+++ src/main/java/net/smartplan/fitness/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package net.smartplan.fitness.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import net.smartplan.fitness.dto.*;
 import net.smartplan.fitness.entity.CaloriePlan;
 import net.smartplan.fitness.entity.MacronutrientFood;
@@ -14,14 +15,16 @@ import net.smartplan.fitness.util.ModelMapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.*;
+import javax.crypto.spec.DESedeKeySpec;
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.security.spec.KeySpec;
+import java.util.*;
 import java.util.stream.Collectors;
 
+
 @Service
+@Slf4j
 @Transactional(rollbackOn = Exception.class)
 public class UserServiceImpl implements UserService {
 
@@ -30,6 +33,12 @@ public class UserServiceImpl implements UserService {
     private final MacronutrientFoodRepository macronutrientFoodRepository;
     private final ModelMapperUtil modelMapperUtil;
     private final AddressRepository addressRepository;
+
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DEEDED_ENCRYPTION_SCHEME = "DESede";
+    private Cipher cipher;
+    byte[] arrayBytes;
+    SecretKey key;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository,
@@ -42,29 +51,51 @@ public class UserServiceImpl implements UserService {
         this.macronutrientFoodRepository = macronutrientFoodRepository;
         this.modelMapperUtil = modelMapperUtil;
         this.addressRepository = addressRepository;
+
+        try {
+            String myEncryptionKey = "H.D.SACHIN DILSHAN NANDANA";
+            arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+            KeySpec ks = new DESedeKeySpec(arrayBytes);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(DEEDED_ENCRYPTION_SCHEME);
+            cipher = Cipher.getInstance(DEEDED_ENCRYPTION_SCHEME);
+            key = skf.generateSecret(ks);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
     }
 
     @Override
     public UserDTO register(UserDTO userDTO) {
+        try {
 
-        User user = userRepository.save(modelMapperUtil.convertToEntity(userDTO));
-        UserAddress address = modelMapperUtil.convertToEntity(userDTO.getAddress());
-        address.setUserId(user);
-        addressRepository.save(address);
-        userDTO.getCaloriePlanList().forEach(detail -> {
-            CaloriePlan caloriePlan = modelMapperUtil.convertToEntity(detail);
-            caloriePlan.setUserId(user);
-            caloriePlanRepository.save(caloriePlan);
-        });
-        userDTO.getMacronutrientFoodList().forEach(detail -> {
-            MacronutrientFood food = modelMapperUtil.convertToEntity(detail);
-            food.setUserId(user);
-            macronutrientFoodRepository.save(food);
-        });
-        UserDTO dto = modelMapperUtil.convertToDTO(user);
-        dto.setAddress(modelMapperUtil.convertToDTO(address));
-        return dto;
+            userDTO.setPassword(encryptPassword(userDTO.getPassword()));
+
+            User user = userRepository.save(modelMapperUtil.convertToEntity(userDTO));
+            UserAddress address = modelMapperUtil.convertToEntity(userDTO.getAddress());
+            address.setUserId(user);
+            addressRepository.save(address);
+            userDTO.getCaloriePlanList().forEach(detail -> {
+                CaloriePlan caloriePlan = modelMapperUtil.convertToEntity(detail);
+                caloriePlan.setUserId(user);
+                caloriePlanRepository.save(caloriePlan);
+            });
+            userDTO.getMacronutrientFoodList().forEach(detail -> {
+                MacronutrientFood food = modelMapperUtil.convertToEntity(detail);
+                food.setUserId(user);
+                macronutrientFoodRepository.save(food);
+            });
+            UserDTO dto = modelMapperUtil.convertToDTO(user);
+            dto.setAddress(modelMapperUtil.convertToDTO(address));
+            return dto;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return new UserDTO();
+
     }
+
 
     @Override
     public boolean checkEmail(String email) {
@@ -75,27 +106,35 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO login(UserDTO userDTO) {
-        userDTO.setCreated(new Date());
-        User user = userRepository.findByEmailAndPassword(userDTO.getEmail(), userDTO.getPassword());
-        if (user != null && user.getStatus().equals("ACTIVE")) {
-            long numOfDays = new Date().getTime() - user.getCreated().getTime();
-            List<CaloriePlanDTO> caloriePlans = user.getCaloriePlanCollection().stream().map(modelMapperUtil::convertToDTO).
-                    collect(Collectors.toList());
-            List<MacronutrientFoodDTO> macronutrientFoodDTOS = user.getMacronutrientFoodCollection().stream().
-                    map(modelMapperUtil::convertToDTO).collect(Collectors.toList());
-            UserDTO dto = modelMapperUtil.convertToDTO(user);
-            dto.setCaloriePlanList(caloriePlans);
-            dto.setNumOfDays((numOfDays / (1000 * 60 * 60 * 24)) % 365);
-            dto.setSuccess(true);
-            dto.setAddress(modelMapperUtil.convertToDTO(user.getUserAddressCollection().get(0)));
-            dto.setMacronutrientFoodList(macronutrientFoodDTOS);
-            return dto;
-        } else {
-            UserDTO dto = new UserDTO();
-            dto.setSuccess(false);
-            return dto;
+        try {
+            userDTO.setCreated(new Date());
+            User user = userRepository.findByEmail(userDTO.getEmail());
+            if (decryptPassword(user.getPassword()).equals(userDTO.getPassword())) {
+                if (user.getStatus().equals("ACTIVE")) {
+                    long numOfDays = new Date().getTime() - user.getCreated().getTime();
+                    List<CaloriePlanDTO> caloriePlans = user.getCaloriePlanCollection().stream().map(modelMapperUtil::convertToDTO).
+                            collect(Collectors.toList());
+                    List<MacronutrientFoodDTO> macronutrientFoodDTOS = user.getMacronutrientFoodCollection().stream().
+                            map(modelMapperUtil::convertToDTO).collect(Collectors.toList());
+                    UserDTO dto = modelMapperUtil.convertToDTO(user);
+                    dto.setCaloriePlanList(caloriePlans);
+                    dto.setNumOfDays((numOfDays / (1000 * 60 * 60 * 24)) % 365);
+                    dto.setSuccess(true);
+                    dto.setAddress(modelMapperUtil.convertToDTO(user.getUserAddressCollection().get(0)));
+                    dto.setMacronutrientFoodList(macronutrientFoodDTOS);
+                    return dto;
+                } else {
+                    UserDTO dto = new UserDTO();
+                    dto.setSuccess(false);
+                    return dto;
+                }
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
 
+        return new UserDTO();
 
     }
 
@@ -218,4 +257,32 @@ public class UserServiceImpl implements UserService {
             caloriePlanRepository.save(caloriePlan);
         });
     }
+
+    public String encryptPassword(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = Base64.getEncoder().encodeToString(encryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+
+
+    public String decryptPassword(String encryptedString) {
+        String decryptedText = null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.getDecoder().decode(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText = new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+
 }
